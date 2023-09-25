39a342fe101841693af05dcc2659ff47f623a5cd
Dung Paulus CTS <linhtam2001a@gmail.com>
2023-09-25 20:45:51 +0700

    Add project files.

---
+++ Data/ApplicaitonDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuanLyHangHoa.Models;
+
+namespace QuanLyHangHoa.Data
+{
+    public class ApplicaitonDbContext
+    {
+        public class ApplicationDbContext : DbContext
+        {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+            {
+
+            }
+
+            public DbSet<HangHoaModel> Employees { get; set; }
+        }
+    }
+}
---
+++ Models/HangHoaModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuanLyHangHoa.Models
+{
+    public class HangHoaModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Ma { get; set; }
+        public string Ten { get; set; }
+        public double? GIa { get; set; }
+
+    }
+}
